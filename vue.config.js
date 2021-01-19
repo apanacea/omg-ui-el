78ced0565aedc823c5c6c07b46d129f0357faa6e
wupan01 <wupan01@baidu.com>
2021-01-20 01:03:11 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+const webpack = require('webpack')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const productionGzipExtensions = ['js', 'css']
+
+module.exports = {
+    productionSourceMap: false,
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+                '@i': path.resolve(__dirname, './src/assets'),
+            }
+        },
+        plugins: [
+            // Ignore all locale files of moment.js
+            new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+
+            // 配置compression-webpack-plugin压缩
+            new CompressionWebpackPlugin({
+                algorithm: 'gzip',
+                test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+                threshold: 10240,
+                minRatio: 0.8
+            }),
+            // new webpack.optimize.LimitChunkCountPlugin({
+            //     maxChunks: 5,
+            //     minChunkSize: 100
+            // })
+        ]
+
+    }
+}
